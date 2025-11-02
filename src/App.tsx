9261c3d74724f931271625b11b55341eccf009c5
SebastianKarpeta <sebastian@karpeta.org>
2025-11-02 14:03:42 +0100

    Adding changes homepage.

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,24 @@
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Login from './Login.jsx';
 
 function App() {
-  return (
+
+
+    const urlSuffix = process.env.NODE_ENV === 'production'
+        ? '/heaven-gallery'
+        : ''
+
+    return (
     <div className="App">
         <BrowserRouter>
             <Routes>
-                <Route path="/heaven-gallery/" element={<Login />} />
+                <Route path={`${urlSuffix}/login`} element={<Login />} />
             </Routes>
         </BrowserRouter>
     </div>
-  );
+    );
 }
 
 export default App;
