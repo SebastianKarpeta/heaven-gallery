f67753fb2fc0c0de801a3ac4de8d68ebc6a02e24
SebastianKarpeta <sebastian@karpeta.org>
2025-11-02 13:43:38 +0100

    Adding changes homepage.

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Login />} />
+                <Route path="/heaven-gallery/" element={<Login />} />
             </Routes>
         </BrowserRouter>
     </div>
